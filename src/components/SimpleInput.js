bb5ff5df130ac6f63dfc5dab7ffca1bd71b4a0e0
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-03 23:51:11 +0430

    Some refactoring

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,35 +1,30 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
+  };
+
+  const nameInputBlurHandler = (e) => {
+    setEnteredNameTouched(true);
   };
 
   const formSubmitionHandler = (e) => {
     e.preventDefault();
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return;
-    }
-
-    setEnteredNameIsValid(true);
-    // 2 Different ways of reading input value
-    console.log(enteredName);
-    console.log(nameInputRef.current.value);
+    if (!enteredNameIsValid) return;
 
     // Reseting the input
     setEnteredName("");
-    // nameInputRef.current.value = ""; // NOT good and eligent
+    setEnteredNameTouched(false);
   };
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
@@ -44,7 +39,7 @@ const SimpleInput = (props) => {
           id="name"
           onChange={nameInputChangeHandler}
           value={enteredName} // RESET
-          ref={nameInputRef}
+          onBlur={nameInputBlurHandler}
         />
         {nameInputIsInvalid && <p className="error-text">Name is not valid</p>}
       </div>
