e3ebb2df3c06c693b50e331c9f5201cc65d8a413
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-03 23:06:47 +0430

    2-ways of getting input value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,36 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmitionHandler = (e) => {
+    e.preventDefault();
+
+    // 2 Different ways of reading input value
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+
+    // Reseting the input
+    setEnteredName("");
+    // nameInputRef.current.value = ""; // NOT good and eligent
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName} // RESET
+          ref={nameInputRef}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
