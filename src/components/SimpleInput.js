ccf522ba80e9737de6493318f6f4af0046ab7a28
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-03 23:35:58 +0430

    Simple validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,8 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
@@ -10,7 +12,14 @@ const SimpleInput = (props) => {
 
   const formSubmitionHandler = (e) => {
     e.preventDefault();
+    setEnteredNameTouched(true);
 
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
     // 2 Different ways of reading input value
     console.log(enteredName);
     console.log(nameInputRef.current.value);
@@ -20,9 +29,15 @@ const SimpleInput = (props) => {
     // nameInputRef.current.value = ""; // NOT good and eligent
   };
 
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <form onSubmit={formSubmitionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           type="text"
@@ -31,6 +46,7 @@ const SimpleInput = (props) => {
           value={enteredName} // RESET
           ref={nameInputRef}
         />
+        {nameInputIsInvalid && <p className="error-text">Name is not valid</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
